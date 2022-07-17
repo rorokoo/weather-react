a5de11039137b47a462efd045a32bf7b8b0cd948
Rorokoo <rorokoo@gmail.com>
2022-07-17 14:31:04 +0300

    removed ReactGA

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Weather from "./Weather";
 import Container from "react-bootstrap/Container";
 import React from "react";
 import GitLink from "./GitLink";
-import ReactGA from "react-ga";
-
-const TRACKING_ID = "G-07JLFZW91M";
-ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
